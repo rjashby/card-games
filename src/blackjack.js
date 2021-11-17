d8adb043aa5b66b488073edcaecdb1b7bbe0fee8
Ryan Ashby <rjapsu160@gmail.com>
2021-11-17 13:45:03 -0800

    add player/dealer score function to blackjack.js.
    Co-authored-by: Hannah Young <hannah@corgibyte.com>
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>

--- src/blackjack.js
+++ src/blackjack.js
@@ -3,14 +3,16 @@ export default class Blackjack {
   constructor() {
     this.deckId = CardService.getCards(6);
     this.deal();
+    this.playerScore = 0;
+    this.dealerScore = 0;
   }
 
   deal() {
     this.playerHand = [];
     this.dealerHand = [];
     this.drawCard(playerHand);
-    this.drawCard(playerHand);
     this.drawCard(dealerHand);
+    this.drawCard(playerHand);
     this.drawCard(dealerHand);
   }
 
@@ -20,7 +22,87 @@ export default class Blackjack {
     if (parseInt(response.remaining) < 15) {
       CardService.shuffleDeck(this.deckId);
     }
+    this.updateScores();
   }
+
+  updateScores() {
+    //Player score
+    let scoreSum = 0;
+    let aces = 0;
+    this.playerHand.forEach(function(card) {
+      let cardValue;
+      switch (card.value) {
+        case 'KING':
+        case 'QUEEN':
+        case 'JACK':
+        case '10':
+          cardValue = 10;
+          break;
+        case '9':
+        case '8':
+        case '7':
+        case '6':
+        case '5':
+        case '4':
+        case '3':
+        case '2':
+          cardValue = parseInt(card.value);
+          break;
+        case 'ACE':
+          aces++;
+          break;
+      }
+      scoreSum += cardValue;
+    });
+    if (aces > 0) {
+      for (let i = 0; i <= aces; i++) {
+        if (scoreSum > 10) {
+          scoreSum += 1;
+        } else {
+          scoreSum += 11;
+        }
+      }      
+    }
+    this.playerScore = scoreSum;
+    //Dealer score
+    scoreSum = 0;
+    aces = 0;
+    this.playerHand.forEach(function(card) {
+      let cardValue;
+      switch (card.value) {
+        case 'KING':
+        case 'QUEEN':
+        case 'JACK':
+        case '10':
+          cardValue = 10;
+          break;
+        case '9':
+        case '8':
+        case '7':
+        case '6':
+        case '5':
+        case '4':
+        case '3':
+        case '2':
+          cardValue = parseInt(card.value);
+          break;
+        case 'ACE':
+          aces++;
+          break;
+      }
+      scoreSum += cardValue;
+    });
+    if (aces > 0) {
+      for (let i = 0; i <= aces; i++) {
+        if (scoreSum > 10) {
+          scoreSum += 1;
+        } else {
+          scoreSum += 11;
+        }
+      }      
+    }
+    this.dealerScore = scoreSum;
+  }  
 
   playerTurn(hit) {
     //TODO
